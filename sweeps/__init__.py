0cc1f3ddae58b36321c866a63aceb3d8503ded1f
Alen Senanian <asenania@Alens-MacBook-Pro.local>
2019-12-22 11:44:26 -0500

    typo

--- sweeps/__init__.py
+++ sweeps/__init__.py
@@ -1,7 +1,7 @@
 from .setup_sweep import create_rfs, delete_rfs
 from .run_sweep import run_sweep
 from .sweep_utils import query_status, read_params
-from .sweep_analysis import get_data, get_DataFrame
+from .sweeps_analysis import get_data, get_DataFrame
 
 __all__ = ["create_rfs", "delete_rfs", "run_sweep", "query_status",\
     "read_params"]
